0e9e3cefbc8322051b692e44964673bb6991135d
Connie Ho <hoconnie@seas.upenn.edu>
2012-09-15 17:59:47 -0400

    News feed layout

--- src/upenn/pennapps2012f/NewsFeedActivity.java
+++ src/upenn/pennapps2012f/NewsFeedActivity.java
@@ -1,8 +1,16 @@
 package upenn.pennapps2012f;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Feed that displays all notifications/events
@@ -12,6 +20,8 @@ import android.view.Window;
  *
  */
 public class NewsFeedActivity extends Activity {
+	private ListView listView;
+	private ArrayAdapter<String> listAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,27 @@ public class NewsFeedActivity extends Activity {
 		// Remove title bar
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.feed_activity);
+		
+		// Get listView resource
+		listView = (ListView) findViewById(R.id.feed_list);
+		
+		// Create and populate list of story titles
+		String[] titles = new String[] {
+				"Re: PennApps logistics",
+				"Want to get lunch later today?",
+				"Charles Kong wrote on your timeline",
+				"What's our 552 homework?",
+				"Jinyan Cao liked your status",
+				"Re: 501 meeting"
+		};
+		ArrayList<String> titleList = new ArrayList<String>();
+		titleList.addAll(Arrays.asList(titles));
+		
+		// Create ArrayAdapter using the title list
+		listAdapter = new ArrayAdapter<String>(this, R.layout.feed_row, R.id.row_title, titleList);
+		
+		// Set ArrayAdapter as ListView's adapter
+		listView.setAdapter(listAdapter);
+		
 	}
 }
